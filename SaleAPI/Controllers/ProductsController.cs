e84eda974d725f67e27365b2da8bb9153f0cf78c
Turbo Laser <galiakberovigor@gmail.com>
2022-11-29 18:52:19 +0300

    real fin first controller

--- SaleAPI/Controllers/ProductsController.cs
+++ SaleAPI/Controllers/ProductsController.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SaleAPI.DataAccess;
 using SaleAPI.Models;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SaleAPI.Controllers
 {
-    [Route("SaleAPI/Products")]
+    [Route("SaleAPI/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : Controller
     {
         private readonly SaleAPIDataContext context;
 
@@ -19,12 +22,72 @@ namespace SaleAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts() 
+        [Route("GetBy/{id}")]
+        public IActionResult GetProductById(int id) 
         {
-            return this.context.Products;
+
+            var product = this.ProductById(id);
+
+            if (product == null) 
+            {
+                return NotFound($"Product {id} not found");
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("List")]
+        public IActionResult GetProducts([FromQuery] int num)
+        {
+            var products = this.context.Products.Take(num);
+            return Ok(products);
         }
 
         [HttpPost]
-        public string Post() => "Hello, World!";
+        [Route("Create")]
+        public async Task<Product> AddProduct([FromBody] Product newProduct) 
+        {
+            this.context.Products.Add(newProduct);
+            await this.context.SaveChangesAsync();
+            return newProduct;
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("DeleteBy/{id}")]
+        public async Task<IActionResult> DeleteProductById(int id)
+        {
+            var product = this.ProductById(id);
+            
+            if (product == null)
+            {
+                return BadRequest($"Product {id} doesn't exist");            
+            }
+
+            this.context.Products.Remove(product);
+            await this.context.SaveChangesAsync();
+            return Ok(product);
+
+        }
+
+        [HttpPut]
+        [Route("UpdateBy/{id}")]
+        public async Task<IActionResult> UpdateProductById(int id, [FromBody] Product product)        
+        {
+            var oldProduct = this.ProductById(id);
+
+            if (oldProduct == null)
+            {
+                return BadRequest($"Product {id} doesn't exist");
+            }
+
+            this.context.Products.Update(product);
+            await this.context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        private Product ProductById(int id) => this.context.Products.FirstOrDefault(p => p.Id == id);
+
     }
 }
