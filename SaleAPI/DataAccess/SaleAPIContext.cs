410333f8b7e8dd2f6911b775d209bea699d506f1
Turbo Laser <galiakberovigor@gmail.com>
2022-12-01 01:20:13 +0300

    add Seed DataBase

--- SaleAPI/DataAccess/SaleAPIContext.cs
+++ SaleAPI/DataAccess/SaleAPIContext.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using SaleAPI.Models;
+using System.Collections.Generic;
 
 namespace SaleAPI.DataAccess
 {
     public class SaleAPIDataContext : DbContext
     {
-        public SaleAPIDataContext(DbContextOptions<SaleAPIDataContext> options) : base(options) { }
-
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Buyer> Buyers { get; set; }
@@ -14,5 +14,59 @@ namespace SaleAPI.DataAccess
         public DbSet<Sale> Sales { get; set; }
 
         public DbSet<SalesPoint> SalesPoints { get; set; }
+
+        public SaleAPIDataContext(DbContextOptions<SaleAPIDataContext> options) : base(options) 
+        {
+            Database.EnsureDeleted();
+            Database.Migrate(); // Database-update
+            this.SeedMockDb(this);
+        }
+
+        protected void SeedMockDb(SaleAPIDataContext context)
+        {
+            context.Buyers.AddRange(
+                 new Buyer() { Name = "Ivan Fedorov", SalesIds = new List<SaleIdE>() },
+                 new Buyer() { Name = "David Mahov", SalesIds = new List<SaleIdE>() },
+                 new Buyer() { Name = "Maria Lungina", SalesIds = new List<SaleIdE>() },
+                 new Buyer() { Name = "Lily Maksacova", SalesIds = new List<SaleIdE>()},
+                 new Buyer() { Name = "Rulon Oboev", SalesIds = new List<SaleIdE>()},
+                 new Buyer() { Name = "Armen Pashinyan", SalesIds = new List<SaleIdE>()},
+                 new Buyer() { Name = "Olga Smetanina", SalesIds = new List<SaleIdE>()},
+                 new Buyer() { Name = "Farida Aleshkova", SalesIds = new List<SaleIdE>()}
+                 );
+
+            context.Products.AddRange(
+                new Product() { Name = "GK61-Keyboard", Price = 45.99m },
+                new Product() { Name = "HHKb-Keyboard", Price = 78.99m },
+                new Product() { Name = "jj41-Keyboard", Price = 25.45m },
+                new Product() { Name = "GK68-Keyboard", Price = 50.00m },
+                new Product() { Name = "BlackTKL", Price = 105.95m },
+                new Product() { Name = "BM16-Keypad", Price = 15.99m },
+                new Product() { Name = "GMKCherrySpace Keycaps", Price = 25.55m },
+                new Product() { Name = "Metal Plate", Price = 25.33m },
+                new Product() { Name = "logimech", Price = 9.99m },
+                new Product() { Name = "SuperGameingKeyboard", Price = 5.99m }
+                );
+
+            var P1 = new ProvidedProduct() { ProductId = 1, ProductQuantity = 10 };
+            var P2 = new ProvidedProduct() { ProductId = 2, ProductQuantity = 6 };
+            var P3 = new ProvidedProduct() { ProductId = 3, ProductQuantity = 15 };
+            var P4 = new ProvidedProduct() { ProductId = 4, ProductQuantity = 3 };
+            var P5 = new ProvidedProduct() { ProductId = 5, ProductQuantity = 4 };
+            var P6 = new ProvidedProduct() { ProductId = 6, ProductQuantity = 12 };
+            var P7 = new ProvidedProduct() { ProductId = 7, ProductQuantity = 12 };
+            var P8 = new ProvidedProduct() { ProductId = 8, ProductQuantity = 5 };
+            var P9 = new ProvidedProduct() { ProductId = 9, ProductQuantity = 62 };
+            var P10 = new ProvidedProduct() { ProductId = 10, ProductQuantity = 55 };
+
+            context.SalesPoints.AddRange(
+                new SalesPoint() { Name = "KeyboardState", ProvidedProducts = new List<ProvidedProduct>() { P1, P2, P3 } },
+                new SalesPoint() { Name = "LocalKeyboards", ProvidedProducts = new List<ProvidedProduct>() { P2, P4, P5, P6 } },
+                new SalesPoint() { Name = "KeyboardGeeks", ProvidedProducts = new List<ProvidedProduct>() { P2, P5, P7, P8} },
+                new SalesPoint() { Name = "Keyboards for everyone", ProvidedProducts = new List<ProvidedProduct>() { P9, P10 } }
+                );
+
+            context.SaveChanges();
+        }
     }
 }
