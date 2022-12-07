e31834bd8415ca5d131b7cbc7c91441c292ba2e8
Turbo Laser <galiakberovigor@gmail.com>
2022-12-07 15:57:14 +0300

    Add Rollback if error occurred

--- BuyerAPI/Startup.cs
+++ BuyerAPI/Startup.cs
@@ -29,10 +29,13 @@ namespace BuyerAPI
             services.AddControllers()
                 .AddNewtonsoftJson(opt => 
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BuyerAPI", Version = "v1" });
             });
+
+            services.AddScoped<HttpUtils.HttpUtils, HttpUtils.HttpUtils>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
